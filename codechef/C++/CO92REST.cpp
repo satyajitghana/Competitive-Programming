c05d76ac999f08ee73e113857dd5351b2f8bc1bc
Satyajit Ghana <satyajitghana7@gmail.com>
2019-06-05 21:42:04 +0530

    Add new

--- codechef/C++/CO92REST.cpp
+++ codechef/C++/CO92REST.cpp
@@ -1,37 +1,28 @@
 /* Include Header Files */
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/math/constants/constants.hpp>
-#include <boost/multiprecision/cpp_dec_float.hpp>
-#include <boost/algorithm/string.hpp>
 #include <bits/stdc++.h>
-#include <iostream>
-#include <string>
-#include <regex>
-#include <cmath>
 
 /* Define Macros */
 
-#define MOD 1e9+7
+#define MOD (long long int)(1e9+7)
 #define precision 1e2
 #define pb push_back
 #define pop pop_back
 #define mp make_pair
-#define pint cpp_int
-#define pint_pair pair<pint, pint>
-#define pfloat cpp_dec_float<precision>
 #define lld long long int
 #define CLR(_a, _init) memset(_a, _init, sizeof(_a))
-#define ODD(_num) (((_num)&1)==0?(0):(1))
+#define ODD(_num) (((_num)&2)==0?(0):(1))
 #define EVEN(_num2) (!(((_num2)&1)==0?(0):(1)))
-#define MAXN (lld)(1e6+5)
+#define MAXN (100500)
+//#define MAXN 20
 #define fastio ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL)
 #define print_pairs(_pairs) for (auto _e : _pairs) { cout << _e.first << " " << _e.second << endl; }
 #define fori(_ii, _begin, _end) for (lld _ii = _begin ; _ii < _end ; _ii++)
 #define DEBUG "DEBUG------"
 #define watch(__x) cout << DEBUG << (#__x) << " : " << (__x) << endl
 #define stop_here return 0
+#define ff first
+#define ss second
 
-using namespace boost::multiprecision;
 using namespace std;
 
 template<typename T> inline void SWAP(T &a, T&b) { T temp = b; b = a; a = temp; }
@@ -47,7 +38,27 @@ template<typename T> inline void print_arr(T arr[], int len) { fori(i, 0, len) {
 
 /* Let the Games Begin */
 
-int A[MAXN];
+int A[MAXN], N;
+int dx[MAXN];
+int in_i[MAXN], out_i[MAXN];
+int in_d[MAXN], out_d[MAXN];
+
+vector<pair<int, int> > merge(int in[MAXN], int out[MAXN]) {
+    vector<pair<int, int> > segs;
+    int cur = -1;
+    int balance = 0;
+    for(int i = 1; i <= N; ++i) {
+        if (cur == -1 && in[i] == 0) continue;
+        balance += in[i];
+        if (balance > 0 && cur == -1) cur = i;
+        balance -= out[i];
+        if (balance == 0 && cur != -1) {
+            segs.pb({cur, i});
+            cur = -1;
+        }
+    }
+    return segs;
+}
 
 int main() {
     fastio;
@@ -56,14 +67,180 @@ int main() {
     cin >> T;
 
     while (T--) {
-        int N, M, K;
+next:
+        int M, K;
         cin >> N >> M >> K;
+
+        for (int i = 1 ; i <= N ; i++) {
+            cin >> A[i];
+            in_i[i] = out_i[i] = 0;
+            in_d[i] = out_d[i] = 0;
+        }
+
+        // vector<pair<int, int> > i_conds;
+        // vector<pair<int, int> > d_conds;
 
         while (M--) {
             char type;
             int L, R;
             cin >> type >> L >> R;
+            if (type == 'I') {
+                // i_conds.pb({L, R});
+                in_i[L]++;
+                out_i[R]++;
+            } else {
+                // d_conds.pb({L, R});
+                in_d[L]++;
+                out_d[R]++;
+            }
         }
+        // sort the increasing and decreasing conditions based on the staring index
+        // sort(i_conds.begin(), i_conds.end());
+        // sort(d_conds.begin(), d_conds.end());
+
+        // find the segments
+
+        vector<pair<int, int> > i_segs = merge(in_i, out_i);
+        vector<pair<int, int> > d_segs = merge(in_d, out_d);
+
+        // vector<pair<int, int> > i_segs;
+
+        // int begin = i_conds[0].ff;
+        // int end = i_conds[0].ss;
+
+        // for (auto e: i_conds) {
+        //     if (e.ff < end) {
+        //         end = e.ss;
+        //     } else {
+        //         i_segs.pb({begin, end});
+        //         begin = e.ff;
+        //         end = e.ss;
+        //     }
+        // }
+
+        // vector<pair<int, int> > d_segs;
+
+        // begin = d_conds[0].ff;
+        // end = d_conds[0].ss;
+
+        // for (auto e: d_conds) {
+        //     if (e.ff < end) {
+        //         end = e.ss;
+        //     } else {
+        //         d_segs.pb({begin, end});
+        //         begin = e.ff;
+        //         end = e.ss;
+        //     }
+        // }
+
+        // check if there are any intersections
+        memset(dx, 0, sizeof(dx));
+
+        for (auto seg: i_segs) {
+            dx[seg.ff]++;
+            dx[seg.ss]--;
+        }
+
+        for (auto seg: d_segs) {
+            dx[seg.ff]++;
+            dx[seg.ss]--;
+        }
+
+        int cnt = 0;
+        int flag = 0;
+        for (int i = 1; i <= N ; i++) {
+            cnt += dx[i];
+
+            if (cnt >= 2) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            cout << 0 << endl;
+            continue;
+        }
+
+        memset(dx, 0, sizeof(dx));
+
+        for (auto seg: i_segs) {
+            dx[seg.ff]++;
+            dx[seg.ss]--;
+        }
+
+        for (auto seg: d_segs) {
+            dx[seg.ff]--;
+            dx[seg.ss]++;
+        }
+
+        for (int i = 1 ; i <= N ; i++) {
+            dx[i] += dx[i-1];
+        }
+
+        int res = 1;
+        int C = N;
+
+        for (int i = 1; i < N ; ) {
+            if (dx[i] == 0) {
+                i++;
+                continue;
+            }
+
+            // find the bounds
+
+            int j = i;
+            while(dx[j] != 0 && j < N) ++j;
+
+            int idx = -1;
+
+            for (int k = i; k <= j; k++) {
+                if (A[k] != -1) idx = k;
+            }
+
+            C -= j - i + 1;
+
+            if (idx == -1) {
+                for (int k = i; k <= j ; k++) {
+                    A[k] = 1;
+                }
+
+                int mx = 0, mn = 0, C = 0;
+
+                for (int k = i; k < j ; k++) {
+                    C += dx[k];
+                    mx = max(mx, C);
+                    mn = min(mn, C);
+                }
+
+                if (mx-mn + 1 > K) res = 0;
+                res = (1LL * res * (K - mx + mn)) % MOD;
+            } else {
+                for (int k = idx - 1; k >= i ; k--) {
+                    int x = A[k+1] - dx[k];
+                    if (A[k] != -1 && A[k] != x) res = 0;
+                    A[k] = x;
+                    if (A[k] < 1 || A[k] > K) res = 0;
+                }
+                for (int k = idx + 1 ; k <= j ; k++) {
+                    int x = A[k-1] + dx[k-1];
+                    if (A[k] != -1 && A[k] != x) res = 0;
+                    A[k] = x;
+                    if (A[k] < 1 || A[k] > K) res = 0;
+                }
+            }
+
+            i = j;
+        }
+
+
+        for (int i = 1; i <= N ; i++) {
+            if (A[i] == -1) res = (1LL * res * K) % MOD;
+        }
+
+        cout << res << endl;
+        // stop_here;
+
     }
 
     return 0;
---
+++ codechef/C++/CO92RESTEDITO.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 20;
+const int mod = 1e9 + 7;
+
+int n, m, k;
+int a[maxn];
+int in_i[maxn], out_i[maxn];
+int in_d[maxn], out_d[maxn];
+int delta[maxn];
+
+vector < pair <int, int> > merge(int in[maxn], int out[maxn]) {
+    vector < pair <int, int> > segs;
+    int cur = -1;
+    int balance = 0;
+    for(int i = 1; i <= n; ++i) {
+        if (cur == -1 && in[i] == 0) continue;
+        balance += in[i];
+        if (balance > 0 && cur == -1) cur = i;
+        balance -= out[i];
+        if (balance == 0 && cur != -1) {
+            segs.push_back(make_pair(cur, i));
+            cur = -1;
+        }
+    }
+    return segs;
+}
+
+int solve() {
+    cin >> n >> m >> k;
+    for(int i = 1; i <= n; ++i) {
+        cin >> a[i];
+        in_i[i] = out_i[i] = 0;
+        in_d[i] = out_d[i] = 0;
+    }
+    while(m--) {
+        char d;
+        int l, r;
+        cin >> d >> l >> r;
+        if (d == 'I') {
+            in_i[l]++;
+            out_i[r]++;
+        } else {
+            in_d[l]++;
+            out_d[r]++;
+        }
+    }
+
+    vector < pair <int, int> > inc = merge(in_i, out_i);
+    vector < pair <int, int> > dec = merge(in_d, out_d);
+    memset(delta, 0, sizeof delta);
+
+    for(auto seg: inc) {
+        delta[seg.first]++;
+        delta[seg.second]--;
+    }
+    for(auto seg: dec) {
+        delta[seg.first]++;
+        delta[seg.second]--;
+    }
+
+    int s = 0;
+    for(int i = 1; i <= n; ++i) {
+        s += delta[i];
+        if (s == 2) return 0;
+    }
+    memset(delta, 0, sizeof delta);
+    for(auto seg: inc) {
+        delta[seg.first]++;
+        delta[seg.second]--;
+    }
+    for(auto seg: dec) {
+        delta[seg.first]--;
+        delta[seg.second]++;
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        delta[i] += delta[i - 1];
+    }
+    int res = 1;
+    int c = n;
+    for(int i = 1; i < n; ) {
+        if (delta[i] == 0) {
+            i++;
+            continue;
+        }
+        int j = i;
+        while(j < n && delta[j] != 0) j++;
+
+        int idx = -1;
+        for(int k = i; k <= j; ++k) {
+            if (a[k] != -1) idx = k;
+        }
+        c -= j - i + 1;
+        if (idx == -1) {
+            for(int k = i; k <= j; ++k) a[k] = 1;
+            int mx = 0, mn = 0, c = 0;
+            for(int k = i; k < j; ++k) {
+                c += delta[k];
+                mx = max(mx, c);
+                mn = min(mn, c);
+            }
+            if (mx - mn + 1 > k) res = 0;
+            res = (1LL * res * (k - mx + mn)) % mod;
+        } else {
+            for(int k = idx - 1; k >= i; --k) {
+                int x = a[k + 1] - delta[k];
+                if (a[k] != -1 && a[k] != x) res = 0;
+                a[k] = x;
+                if (a[k] < 1 || a[k] > ::k) res = 0;
+            }
+            for(int k = idx + 1; k <= j; ++k) {
+                int x = a[k - 1] + delta[k - 1];
+                if (a[k] != -1 && a[k] != x) res = 0;
+                a[k] = x;
+                if (a[k] < 1 || a[k] > ::k) res = 0;
+            }
+        }
+        i = j;
+    }
+    for(int i = 1; i <= n; ++i) {
+        if (a[i] == -1) res = (1LL * res * k) % mod;
+    }
+    return res;
+}
+
+int main () {
+    ios_base::sync_with_stdio(0);
+   // freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int T;
+    cin >> T;
+    while(T--) {
+        cout << solve() << endl;
+    }
+    return 0;
+}
