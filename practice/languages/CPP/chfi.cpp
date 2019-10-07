a9fb73bee878f5a0738e237bd3a1cb4c146e519f
vivek <56218493+vivekbadani09@users.noreply.github.com>
2019-10-07 17:10:25 +0530

    Create chfi.cpp

---
+++ practice/languages/CPP/chfi.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+  int x,y,z;
+  srand(time(0));
+x=int ((rand()%3)+1);
+cout<<x<<endl;
+fflush(stdout);
+cin>>y;
+fflush(stdin);
+z=6-(x+y);
+cout<<z<<endl;
+fflush(stdout);
+
+
+
+
+return 0;
+}
