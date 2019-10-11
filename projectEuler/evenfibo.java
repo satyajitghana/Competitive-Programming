4d2ad6ea218320e3b1818640abb41d9ec05fb9c6
Neha Tirumalasetti <42908650+NehaTirumalasetti@users.noreply.github.com>
2019-10-11 23:37:31 +0530

    Update evenfibo.java

--- projectEuler/evenfibo.java
+++ projectEuler/evenfibo.java
@@ -5,6 +5,8 @@ Problem 2:
 	1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 	
 	By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+	
+	https://projecteuler.net/problem=2
  */
 
 public class evenfibo
