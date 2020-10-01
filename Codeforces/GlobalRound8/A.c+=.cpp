88ad3102d60ef9019115338068208ab8bffbe427
Jesus Wahrman <41008968+jesuswr@users.noreply.github.com>
2020-10-01 13:07:38 -0400

    Create A.c+=.cpp

---
+++ Codeforces/GlobalRound8/A.c+=.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<int,pair<int,int>> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+#define ri(a) scanf("%d", &a)
+#define rii(a,b) scanf("%d %d", &a, &b)
+#define riii(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define rl(a) scanf("%lld", &a)
+#define rll(a,b) scanf("%lld %lld", &a, &b)
+#define FOR(i,n,m) for(int i=n; i<m; i++)
+#define ROF(i,n,m) for(int i=n; i>m; i--)
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define F first
+#define S second
+
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 1e18;
+const int maxN = 1e5; // CAMBIAR ESTE
+
+// GJNM
+
+int main(){
+	int t;
+	ri(t);
+	while( t-- ){
+		ll a,b,n;
+		rll(a,b), rl(n);
+		int cnt = 0;
+		while( a <= n && b <= n ){
+			if ( a <= b ){
+				a += b;
+			}
+			else{
+				b += a;
+			}
+			cnt++;
+		}
+		printf("%d\n", cnt);
+	}
+	return 0;
+}
