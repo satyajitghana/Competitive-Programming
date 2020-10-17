adb222e264fab473333b8f2faa67ee4a83527e45
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2020-10-17 13:40:34 +0545

    Update CopyListWithNextRandomPointer.cpp

--- practice/languages/CPP/CopyListWithNextRandomPointer.cpp
+++ practice/languages/CPP/CopyListWithNextRandomPointer.cpp
@@ -1,3 +1,19 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
