c3c4683793d0f2848366aeb78290c914d69e8f30
Satyajit Ghana <satyajitghana7@gmail.com>
2020-10-31 21:53:05 +0530

    Update b.cpp

--- Codeforces/Round352_Div_2/b.cpp
+++ Codeforces/Round352_Div_2/b.cpp
@@ -2,31 +2,25 @@
 using namespace std;
 
 int main() {
-
   int n;
-  cin>>n;
-    string s;
-  cin>>s;
-  int l =s.length();
-  int c=0;
-  if(l<27)
-  {
-        for(int i=0;i<l-1;i++)
-      {
-        for(int j=i+1;j<l;j++)
-        {
-            if(s[i]==s[j]){
-                c++;
-                break;
-            }
+  cin >> n;
+  string s;
+  cin >> s;
+  int l = s.length();
+  int c = 0;
+  if (l < 27) {
+    for (int i = 0; i < l - 1; i++) {
+      for (int j = i + 1; j < l; j++) {
+        if (s[i] == s[j]) {
+          c++;
+          break;
         }
       }
-  }
+    }
+  } else
+    c = -1;
+  if (c < 26)
+    cout << c;
   else
-  c=-1;
-      if(c<26)
-      cout<<c;
-      else
-      cout<<-1;
-
+    cout << -1;
 }
